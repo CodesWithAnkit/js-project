8a3dd7b634c75eca6d8c836fb08e505f389d7569
codeswithankit <ank8804465648@gmail.com>
2020-08-05 01:22:46 +0530

    bookmarks app

--- bookmark-app/script2.js
+++ bookmark-app/script2.js
@@ -1,61 +1,52 @@
-// with booksmarks save in Array
+// With bookMarks save in Object
+
 const modal = document.getElementById('modal')
 const modalShow = document.getElementById('show-modal')
-const closeModal = document.getElementById('close-modal')
+const modalClose = document.getElementById('close-modal')
 const bookmarkForm = document.getElementById('bookmark-form')
 const websiteNameEl = document.getElementById('website-name')
 const websiteUrlEl = document.getElementById('website-url')
-const bookmarkContainer = document.getElementById('bookmarks-container')
+const bookmarksContainer = document.getElementById('bookmarks-container')
 
-let bookmarks = []
+let bookmarks = {}
 
-// show modal function
-const showModal = () => {
+// Show Modal, Focus on Input
+function showModal() {
   modal.classList.add('show-modal')
   websiteNameEl.focus()
 }
 
+// Modal Event Listeners
 modalShow.addEventListener('click', showModal)
-closeModal.addEventListener('click', () => modal.classList.remove('show-modal'))
-// close the modal while click on overlay of the modal
+modalClose.addEventListener('click', () => modal.classList.remove('show-modal'))
 window.addEventListener('click', (e) =>
   e.target === modal ? modal.classList.remove('show-modal') : false
 )
 
-// validate form
-const validate = (nameValue, urlValue) => {
-  const expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/g
+// Validate Form
+function validate(nameValue, urlValue) {
+  const expression = /(https)?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/g
   const regex = new RegExp(expression)
-
   if (!nameValue || !urlValue) {
-    alert('Please Provide Name and Url value')
+    alert('Please submit values for both fields.')
     return false
   }
-
   if (!urlValue.match(regex)) {
-    alert('Please Provide a valide URL')
+    alert('Please provide a valid web address.')
     return false
   }
+  // Valid
   return true
 }
 
-// deleteBookmarks
-const deleteBookmark = (url) => {
-  bookmarks.forEach((bookmark, index) => {
-    if (bookmark.url === url) {
-      bookmarks.splice(index, 1)
-    }
-  })
-  localStorage.setItem('bookmarks', JSON.stringify(bookmarks))
-  fetchBookmarks()
-}
+// Build Bookmarks
+function buildBookmarks() {
+  // Remove all bookmark elements
+  bookmarksContainer.textContent = ''
+  // Build items
+  Object.keys(bookmarks).forEach((id) => {
+    const { name, url } = bookmarks[id]
 
-const buildBookmarks = () => {
-  // Remove Bookmarks from UI
-  bookmarkContainer.textContent = ''
-
-  bookmarks.forEach((bookmark) => {
-    const { name, url } = bookmark
     // Item
     const item = document.createElement('div')
     item.classList.add('item')
@@ -63,7 +54,7 @@ const buildBookmarks = () => {
     const closeIcon = document.createElement('i')
     closeIcon.classList.add('fas', 'fa-times')
     closeIcon.setAttribute('title', 'Delete Bookmark')
-    closeIcon.setAttribute('onclick', `deleteBookmark('${url}')`)
+    closeIcon.setAttribute('onclick', `deleteBookmark('${id}')`)
     // Favicon / Link Container
     const linkInfo = document.createElement('div')
     linkInfo.classList.add('name')
@@ -82,49 +73,65 @@ const buildBookmarks = () => {
     // Append to bookmarks container
     linkInfo.append(favicon, link)
     item.append(closeIcon, linkInfo)
-    bookmarkContainer.appendChild(item)
+    bookmarksContainer.appendChild(item)
   })
 }
 
-// fetchBookmarks
-const fetchBookmarks = () => {
+// Fetch bookmarks
+function fetchBookmarks() {
+  // Get bookmarks from localStorage if available
   if (localStorage.getItem('bookmarks')) {
     bookmarks = JSON.parse(localStorage.getItem('bookmarks'))
   } else {
-    //   create bookmarks in localstorage
-    bookmarks = [
-      {
-        name: 'Google',
-        url: 'www.google.com',
-      },
-    ]
+    // Create bookmarks object in localStorage
+    const id = `http://jacinto.design`
+    bookmarks[id] = {
+      name: 'Jacinto Design',
+      url: 'http://jacinto.design',
+    }
+
     localStorage.setItem('bookmarks', JSON.stringify(bookmarks))
   }
-
   buildBookmarks()
 }
 
-// handle data from form
-const storeBookMarks = (e) => {
+// Delete Bookmark
+function deleteBookmark(id) {
+  // Loop through the bookmarks array
+  if (bookmarks[id]) {
+    delete bookmarks[id]
+  }
+  // Update bookmarks array in localStorage, re-populate DOM
+  localStorage.setItem('bookmarks', JSON.stringify(bookmarks))
+  fetchBookmarks()
+}
+
+function storeBookmark(e) {
   e.preventDefault()
   const nameValue = websiteNameEl.value
   let urlValue = websiteUrlEl.value
   if (!urlValue.includes('http://', 'https://')) {
     urlValue = `https://${urlValue}`
   }
-  if (!validate(nameValue, urlValue)) return false
+  // Validate
+  if (!validate(nameValue, urlValue)) {
+    return false
+  }
+  // Set bookmark object, add to array
   const bookmark = {
     name: nameValue,
     url: urlValue,
   }
-  bookmarks.push(bookmark)
+  bookmarks[urlValue] = bookmark
+  // Set bookmarks in localStorage, fetch, reset input fields
   localStorage.setItem('bookmarks', JSON.stringify(bookmarks))
   fetchBookmarks()
   bookmarkForm.reset()
   websiteNameEl.focus()
 }
 
-bookmarkForm.addEventListener('submit', storeBookMarks)
+// Event Listener
+bookmarkForm.addEventListener('submit', storeBookmark)
 
-// onLoad FetchBookmarks
+// On Load, Fetch Bookmarks
 fetchBookmarks()
